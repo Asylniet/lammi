cac9e405bdb047b51d10a7ff7187bb023912e027
Asylniet <zasylniet@mail.ru>
2024-03-17 14:37:50 +0500

    added phone_number to basked form

--- src/api/appointment.ts
+++ src/api/appointment.ts
@@ -1,86 +1,113 @@
 import { API_URL } from "@/lib/const";
 
 export type Category = {
-  id: number,
-  name: string,
-}
+  id: number;
+  name: string;
+};
 
 export const getCategories = async (branchId: number): Promise<Category[]> => {
   try {
-    const res = await fetch(`${API_URL}/categories/${branchId}`)
-    return res.json()
+    const res = await fetch(`${API_URL}/categories/${branchId}`);
+    return res.json();
   } catch (error) {
     throw error;
   }
-}
+};
 
 export type Service = {
-  id: number,
-  name: string,
-}
+  id: number;
+  name: string;
+};
 
-export const getServices = async (branchId: number, categoryId: number): Promise<Service[]> => {
+export const getServices = async (
+  branchId: number,
+  categoryId: number,
+): Promise<Service[]> => {
   try {
-    const res = await fetch(`${API_URL}/services/${branchId}/${categoryId}`)
-    return res.json()
+    const res = await fetch(`${API_URL}/services/${branchId}/${categoryId}`);
+    return res.json();
   } catch (error) {
     throw error;
   }
-}
+};
 
 export type Speicalist = {
-  id: number,
-  name: string,
-}
+  id: number;
+  name: string;
+};
 
-export const getSpeicalists = async (branchId: number, serviceId: number): Promise<Speicalist[]> => {
+export const getSpeicalists = async (
+  branchId: number,
+  serviceId: number,
+): Promise<Speicalist[]> => {
   try {
-    const res = await fetch(`${API_URL}/employees/${branchId}/${serviceId}`)
-    return res.json()
+    const res = await fetch(`${API_URL}/employees/${branchId}/${serviceId}`);
+    return res.json();
   } catch (error) {
     throw error;
   }
-}
+};
 
 export type CustomDate = {
-  id: number,
-  name: string,
-}
+  id: number;
+  name: string;
+};
 
-export const getDates = async (branchId: number, serviceId: number, specialistId: number): Promise<CustomDate[]> => {
+export const getDates = async (
+  branchId: number,
+  serviceId: number,
+  specialistId: number,
+): Promise<CustomDate[]> => {
   try {
-    const res = await fetch(`${API_URL}/dates/${branchId}/${serviceId}/${specialistId}`)
-    return res.json()
+    const res = await fetch(
+      `${API_URL}/dates/${branchId}/${serviceId}/${specialistId}`,
+    );
+    return res.json();
   } catch (error) {
     throw error;
   }
-}
+};
 
 export type Time = {
-  id: number,
-  name: string,
-}
+  id: number;
+  name: string;
+};
 
-export const getTimes = async (branchId: number, serviceId: number, specialistId: number, date: string): Promise<Time[]> => {
+export const getTimes = async (
+  branchId: number,
+  serviceId: number,
+  specialistId: number,
+  date: string,
+): Promise<Time[]> => {
   try {
-    const res = await fetch(`${API_URL}/hours/${branchId}/${specialistId}/${date}/${serviceId}`)
-    return res.json()
+    const res = await fetch(
+      `${API_URL}/hours/${branchId}/${specialistId}/${date}/${serviceId}`,
+    );
+    return res.json();
   } catch (error) {
     throw error;
   }
-}
+};
 
 interface Appointment {
-  branch_id: number,
-  category_id?: number,
-  service_id: number,
-  employee_id: number,
-  date_id: number,
-  time_id: number,
-  phone_number: string,
+  branch_id: number;
+  category_id?: number;
+  service_id: number;
+  employee_id: number;
+  date_id: number;
+  time_id: number;
+  phone_number: string;
 }
 
-export const makeAppointment = async ({branch_id, service_id, employee_id, date_id, time_id, phone_number, category_id}: Appointment): Promise<string> => {
+export const makeAppointment = async ({
+  branch_id,
+  service_id,
+  employee_id,
+  date_id,
+  time_id,
+  phone_number,
+  category_id,
+}: Appointment): Promise<string> => {
   try {
     const res = await fetch(`${API_URL}/bookings`, {
       method: "POST",
@@ -96,19 +123,19 @@ export const makeAppointment = async ({branch_id, service_id, employee_id, date_
         time_id,
         phone_number,
       }),
-    }).then(res => res.text())
+    }).then((res) => res.text());
     return res;
   } catch (error) {
     throw error;
   }
-}
+};
 
 interface Order {
   client: {
-    full_name: string,
-    delivery_address: string,
-  },
-  products: { id: number, quantity: number }[],
+    full_name: string;
+    delivery_address: string;
+  };
+  products: { id: number; quantity: number; phone_number: string }[];
 }
 
 export const makeOrder = async (params: Order): Promise<string> => {
@@ -119,24 +146,24 @@ export const makeOrder = async (params: Order): Promise<string> => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(params),
-    }).then(res => res.text())
+    }).then((res) => res.text());
     return res;
   } catch (error) {
     console.log(error);
     throw error;
   }
-}
+};
 
 export type CustomSpecialist = {
-  name: string,
-  position: string,
-}
+  name: string;
+  position: string;
+};
 
 export const getCustomSpecialists = async (): Promise<CustomSpecialist[]> => {
   try {
-    const res = await fetch(`${API_URL}/specialists`)
-    return res.json()
+    const res = await fetch(`${API_URL}/specialists`);
+    return res.json();
   } catch (error) {
     throw error;
   }
-}
+};
--- src/app/modal/basketSlides/second.tsx
+++ src/app/modal/basketSlides/second.tsx
@@ -1,61 +1,91 @@
-import React from 'react'
-import { TotalSum } from './totalSum';
-import Button from '@/components/button';
-import { useSwiper } from 'swiper/react';
-import { makeOrder } from '@/api/appointment';
-import { useMutation } from '@tanstack/react-query';
-import { useProductStore } from '@/store/products';
-import { ArrowSvg } from '@/app/svg/arrow.svg';
+import React from "react";
+import { TotalSum } from "./totalSum";
+import Button from "@/components/button";
+import { useSwiper } from "swiper/react";
+import { makeOrder } from "@/api/appointment";
+import { useMutation } from "@tanstack/react-query";
+import { useProductStore } from "@/store/products";
+import { ArrowSvg } from "@/app/svg/arrow.svg";
+import { formatPhone } from "@/lib/formatPhone";
 
 export const Second = () => {
   const swiper = useSwiper();
-  const [name, setName] = React.useState('');
-  const [address, setAddress] = React.useState('');
-  const products = useProductStore(state => state.products);
+  const [name, setName] = React.useState("");
+  const [address, setAddress] = React.useState("");
+  const [number, setNumber] = React.useState("");
+  const products = useProductStore((state) => state.products);
 
   const mutation = useMutation({
-    mutationKey: ['createOrder'],
+    mutationKey: ["createOrder"],
     mutationFn: makeOrder,
     onSuccess: (data) => {
       window.location.href = data;
-    }
-  })
+    },
+  });
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    mutation.mutate({ 
+    mutation.mutate({
       client: {
         full_name: name,
         delivery_address: address,
       },
-      products: products.map(product => ({
+      products: products.map((product) => ({
         id: product.product.id,
         quantity: product.quantity,
+        phone_number: formatPhone(number),
       })),
-     });
-  }
+    });
+  };
   return (
     <>
-      <h1 className='font-bold uppercase text-center text-xl px-2 mb-4'>Корзина</h1>
-      <div className="bg-accent w-full h-px mb-6"></div>
-      <div className='flex flex-col h-full mx-auto max-w-[600px] px-6'>
+      <h1 className="mb-4 px-2 text-center text-xl font-bold uppercase">
+        Корзина
+      </h1>
+      <div className="bg-accent mb-6 h-px w-full"></div>
+      <div className="mx-auto flex h-full max-w-[600px] flex-col px-6">
         <TotalSum />
-        <button onClick={() => swiper.slidePrev()} className='my-4 w-fit rounded-md flex items-center px-4 py-2 bg-accent/20 gap-2'>
-          <div className="rotate-180 max-w-[15px] flex items-center">
+        <button
+          onClick={() => swiper.slidePrev()}
+          className="bg-accent/20 my-4 flex w-fit items-center gap-2 rounded-md px-4 py-2"
+        >
+          <div className="flex max-w-[15px] rotate-180 items-center">
             <ArrowSvg />
           </div>
           Изменить корзину
         </button>
         <form onSubmit={handleSubmit}>
-          <div className='mb-6'>
-            <label htmlFor='name'>Полное имя</label>
-            <input type="text" placeholder='Имя Фамилия' id='name' value={name} onChange={(e) => setName(e.target.value)} />
+          <div className="mb-6">
+            <label htmlFor="name">Полное имя</label>
+            <input
+              type="text"
+              placeholder="Имя Фамилия"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
-          <div className='mb-6'>
-            <label htmlFor='address'>Адрес доставки</label>
-            <input type="text" placeholder='Адрес' id='address' value={address} onChange={(e) => setAddress(e.target.value)} />
+          <div className="mb-6">
+            <label htmlFor="address">Номер телефона</label>
+            <input
+              type="tel"
+              placeholder="+7 777 777 77 77"
+              id="phone"
+              value={number}
+              onChange={(e) => setNumber(e.target.value)}
+            />
           </div>
-          <div className="w-fit mx-auto mt-12 text-foreground">
+          <div className="mb-6">
+            <label htmlFor="address">Адрес доставки</label>
+            <input
+              type="text"
+              placeholder="Адрес"
+              id="address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </div>
+          <div className="text-foreground mx-auto mt-12 w-fit">
             <Button status={mutation.status} disabled={!name || !address}>
               Оформить заказ
             </Button>
@@ -63,5 +93,5 @@ export const Second = () => {
         </form>
       </div>
     </>
-  )
-}
+  );
+};
